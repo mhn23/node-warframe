7a1c08ce4a73afcae89244123224cc10615cc8b3
Martin Konstantin Herrmann <martin.k.herrmann@me.com>
2016-10-15 22:39:55 +0200

    serval changes and new stuff
    able to collect guild to large file and jsons, to jsons in folder, get
    all players in guilds+id plus some not working tests

--- test/getguildmember.js
+++ test/getguildmember.js
@@ -7,7 +7,7 @@ warframe.login(config.login.username, config.login.password, function(res){
     warframe.getGuild(function(view){
         var guild = JSON.parse(view);
         guild.Members.forEach(function(member){
-            console.log("username:", member.DisplayName.green, "id:", member._id.$id);
+            console.log("username:", member.DisplayName, "id:", member._id.$id);
         });
     });
 });
